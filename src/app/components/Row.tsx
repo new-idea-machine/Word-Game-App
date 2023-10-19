2718189541e396c864094ed7fad0a5705a398959
mike-dalton <michael.j.dalton@gmail.com>
2023-10-19 12:17:05 -0400

    add shake animation for wrong guess

--- src/app/components/Row.tsx
+++ src/app/components/Row.tsx
@@ -1,10 +1,12 @@
 interface RowProps {
   word: string;
   guessed: boolean;
+  wrongGuess: boolean;
 }
 
-export default function Row({ word, guessed }: RowProps) {
+export default function Row({ word, guessed, wrongGuess }: RowProps) {
   const boxFormat = guessed ? "flipRow" : "border-2 border-gray-500";
+  const errorFormat = wrongGuess ? 'wrongGuess' : '';
 
   return (
     <div className="flex justify-center">
@@ -12,7 +14,7 @@ export default function Row({ word, guessed }: RowProps) {
         return (
           <div
             key={i}
-            className={`flex justify-center items-center w-[52px] h-[52px] m-1 text-center uppercase font-extrabold text-[2em] ${boxFormat}`}
+            className={`flex justify-center items-center w-[52px] h-[52px] m-1 text-center uppercase font-extrabold text-[2em] ${boxFormat} ${errorFormat}`}
           >
             {letter}
           </div>
