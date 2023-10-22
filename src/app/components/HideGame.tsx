4871057395880023368c680134f3a7cee82fe5eb
mlestrella843 <98559654+mlestrella843@users.noreply.github.com>
2023-10-22 14:45:13 -0600

    Adding user's achivements

--- src/app/components/HideGame.tsx
+++ src/app/components/HideGame.tsx
@@ -9,7 +9,7 @@ interface Props {
 export default function HideGame({ countdownMilliSeconds, setLaunch }: Props) {
   return (
     <div className="w-full h-full px-4 md:px-24 mt-20 flex flex-col items-center">
-      <p>The next game will be available in:</p>
+      <p className="text-black text-xl font-bold">The next game will be available in:</p>
       <Countdown date={countdownMilliSeconds} onComplete={() => setLaunch(true)} />
     </div>
   );
