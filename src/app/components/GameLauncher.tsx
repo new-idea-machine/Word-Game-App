345e5f60e4df8049f3932de9fb254d77757ea8ee
mike-dalton <michael.j.dalton@gmail.com>
2023-10-25 11:38:14 -0400

    add timer component

--- src/app/components/GameLauncher.tsx
+++ src/app/components/GameLauncher.tsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from "react";
 import GameCountdown from "./GameCountdown";
 import { getCookie } from "cookies-next";
 import Game from "./Game";
+import GameTimer from "./GameTimer";
 
 export default function GameLauncher() {
   const [launch, setLaunch] = useState<boolean | null>(null);
@@ -19,8 +20,10 @@ export default function GameLauncher() {
 
   return (
     <div className="container h-full pt-20 mx-auto">
-      {launch && <Game />}
-      {(launch === false) && <GameCountdown onComplete={() => setLaunch(true)} />}
+      <GameTimer interval={11000} countDirection="down" />
+      <Game />
+      {/* {launch && <Game />}
+      {(launch === false) && <GameCountdown onComplete={() => setLaunch(true)} />} */}
     </div>
   );
 } 
