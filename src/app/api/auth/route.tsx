26115956e0ccac500b5c729f2b72de6947f1a8d5
RJ <randy.j416@yahoo.com>
2023-10-28 14:20:23 -0400

    initial commit, switching to bcryptjs

--- src/app/api/auth/route.tsx
+++ src/app/api/auth/route.tsx
@@ -1,24 +1,50 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-import { NextResponse } from 'next/server'  
- 
+import type { NextApiRequest, NextApiResponse } from "next";
+import { NextResponse } from "next/server";
+
+const bcrypt = require("bcryptjs");
+
 type ResponseData = {
-  message: string
-}
+  message: string;
+};
 
-export async function POST(request: NextApiRequest) {
-  let body;
+type user = { username: string; password: any };
+const mockUsers: user[] = [
+  { username: "Mike", password: bcrypt.hashSync("password", 10) },
+  {
+    username: "Jon",
+    password: "$2a$10$B5fmfHrGGbntV8uKFIJpS.kSDfgXhiwPjABkDyAnuLujQ4GYwAAES",
+  },
+];
 
-  try {
-    // Read the request body and parse it as JSON
-    body = JSON.parse(await request.text());
-  } catch (error) {
-    console.error("Error parsing request body as JSON:", error);
-    return NextResponse.error();
+export async function POST(req: Request, res: any) {
+  const { username, password } = await req.json();
+  console.log(username, password);
+  const validUser = mockUsers.filter((user) => user.username === username)[0];
+  if (!validUser) {
+    return NextResponse.json(
+      { message: "Authentication failed" },
+      { status: 401 }
+    );
   }
 
-  console.log(body);
+  if (validUser) {
+    console.log(validUser);
 
-  return NextResponse.json({ message: 'Hello from Next.js!' });
-
+    const validPass = bcrypt.compareSync(
+      password,
+      validUser.password
+    );
+    console.log("is valid?: ", validPass);
+    if (validPass) {
+      return NextResponse.json(
+        { message: "Authentication passed" },
+        { status: 200 }
+      );
+    } else {
+      return NextResponse.json(
+        { message: "Authentication failed" },
+        { status: 401 }
+      );
+    }
+  }
 }
-
--- src/app/components/Auth.tsx
+++ src/app/components/Auth.tsx
@@ -1,106 +1,44 @@
 "use client";
 
-import { request } from "http";
 import next from "next";
 import { ChangeEvent, FormEvent, useState } from "react";
-const passport = require("passport");
-const LocalStrategy = require("passport-local");
-const crypto = require("crypto");
+const bcrypt = require("bcryptjs");
 
-type user = { username: string; password: string; salt: unknown };
-const mockUsers: user[] = [
-  { username: "Mike", password: "password", salt: crypto.randomBytes(16) },
-  { username: "Jon", password: "password", salt: crypto.randomBytes(16) },
-];
 
 export default function Auth() {
-  passport.use(
-    "mock",
-    new LocalStrategy(function verify(
-      username: string,
-      password: string,
-      cb: Function
-    ) {
-      // db.get('SELECT * FROM users WHERE username = ?', [ username ], function(err: Error, user: user) {
-      //   if (err) { return cb(err); }
-      //   if (!user) { return cb(null, false, { message: 'Incorrect username or password.' }); }
-
-      // });
-
-      const validUser = mockUsers.filter(
-        (user) => user.username === username
-      )[0];
-
-      if (!validUser) {
-        return cb(null, false, { message: "Incorrect username or password." });
-      }
-
-      if (validUser) {
-        console.log(validUser);
-        // crypto.pbkdf2(password, user.salt, 310000, 32, 'sha256', function(err: Error, hashedPassword: any) {
-        //   if (err) { return cb(err); }
-        //   if (!crypto.timingSafeEqual(user.hashed_password, hashedPassword)) {
-        //     return cb(null, false, { message: 'Incorrect username or password.' });
-        //   }
-        //   return cb(null, user);
-        // });
-      }
-    })
-  );
   const [username, setUsername] = useState<String | null>(null);
-  const [password, setPassword] = useState<String | null>(null);
+  const [password, setPassword] = useState<String | null | any>(null);
 
   // function handleUsernameInput(event: ChangeEvent<HTMLInputElement>): void {
   //   setUsername(event.target.value);
   // }
+  // function handlePasswordInput(event: ChangeEvent<HTMLInputElement>): void {
+  //   setPassword(event.target.value);
+  // }
 
   const handleSubmit = async function (event: FormEvent<HTMLFormElement>) {
     event.preventDefault();
-    console.log(username);
-    // passport.authenticate('local', (err: Error, user) => {
-    //   if (err) {
-    //     return next(err)
-    //   }
-
-
-    //   console.log(user)
-    // })
-
-    passport.authenticate("mock", {
-      successFlash: "Welcome back!",
-      failureFlash: "Invalid username or password.",
-      session: true,
-    });
-
     console.log("Submitting:", { username, password });
+    try {
+      const response = await fetch("/api/auth", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: username,
+          password: bcrypt.hashSync(password, 10),
+        }),
+      })
+      const data = await response.json();
+      console.log(data)
+    } catch (err) {
+      // handle error
+      console.log("error!")
+    }
 
-    const response = await fetch('/api/auth', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({username: username, password: password, salt: crypto.randomBytes(16)}),
-    })
-    .then((res) => {return res.text()
-    })
-    .then(res => {
-      console.log("response: ")
-      console.log(res)
-      // console.log(JSON.parse(res.body.text()))
-      
-  
-      // console.log(data)
+  };
 
-    })
-
-
-    
-    
-  }
-
-  function handlePasswordInput(event: ChangeEvent<HTMLInputElement>): void {
-    // setPassword(event.target.value);
-  }
 
   return (
     <div className="flex flex-col p-5 justify-center">
