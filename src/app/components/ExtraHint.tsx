dced2ee83e3d421baabcda9f2433e68304035f74
KovaKreative <eric.kovalevskyy@gmail.com>
2023-10-31 15:13:59 -0400

    Modify ExtraHint component to use outside props

--- src/app/components/ExtraHint.tsx
+++ src/app/components/ExtraHint.tsx
@@ -1,47 +1,37 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import classnames from "classnames";
 
 interface Props {
   hint: string | null;
+  step: number;
   setHint: Function;
-  counter: number;
-  setCounter: Function
+  numOfHints: number;
 }
 
-export default function ExtraHint({ hint, setHint, counter, setCounter }: Props) {
-  
-  const numOfTries = 3;
-
-  const [clicks, setClicks] = useState(numOfTries);
+export default function ExtraHint({ hint, step, setHint, numOfHints }: Props) {
 
   const extraHintFormat = classnames(
 
     "h-12 px-4 text-xl rounded transition",
     {
       "bg-lime-600 cursor-default revealHint": typeof hint === 'string',
-      "bg-sky-600 text-white hover:bg-sky-500": !hint && clicks > 0,
-      "bg-gray-600 hover:bg-gray-600 cursor-not-allowed": clicks <= 0
+      "bg-sky-600 text-white hover:bg-sky-500": !hint && numOfHints > 0,
+      "bg-gray-600 hover:bg-gray-600 cursor-not-allowed": numOfHints <= 0
     }
   );
-
   return (
-    <div>
-      <button
-        className={extraHintFormat}
-        onClick={e => {
-          e.preventDefault();
-          if(clicks <= 0 || !!hint) {
-            // return clicks;
-          }
-          setClicks(prev => prev - 1);
-          setHint(true);
-          setCounter(counter + 1);
-          // return clicks;
-        }}
-        title={clicks <= 0 ? 'You are out of extra hints' : !hint ? 'Click to get an extra hint' : 'Extra hint'}
-        >
-          {hint || (clicks <= 0 ? '❌' : `❔x ${clicks}`)}
-      </button>
-    </div>
+    <button
+      className={extraHintFormat}
+      onClick={e => {
+        e.preventDefault();
+        if (!!hint || numOfHints <= 0) {
+          return;
+        }
+        setHint(true);
+      }}
+      title={numOfHints <= 0 ? 'You are out of extra hints' : !hint ? 'Click to get an extra hint' : 'Extra hint'}
+    >
+      {hint || (numOfHints <= 0 ? '❌' : `❔x ${numOfHints}`)}
+    </button>
   );
 }
