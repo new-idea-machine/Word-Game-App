013c07d8e04bf0fc8894dc9ec0524ac060393ec8
KovaKreative <eric.kovalevskyy@gmail.com>
2023-11-02 17:26:25 -0400

    Replace Hint emojis with SVGs

--- src/app/components/ExtraHint.tsx
+++ src/app/components/ExtraHint.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import classnames from "classnames";
 
 interface Props {
@@ -12,11 +11,11 @@ export default function ExtraHint({ hint, step, setHint, numOfHints }: Props) {
 
   const extraHintFormat = classnames(
 
-    "h-12 px-4 text-xl rounded transition",
+    "h-12 text-xl rounded transition flex items-center justify-center",
     {
       "bg-lime-600 cursor-default revealHint": typeof hint === 'string',
-      "bg-sky-600 text-white hover:bg-sky-500": !hint && numOfHints > 0,
-      "bg-gray-600 hover:bg-gray-600 cursor-not-allowed": numOfHints <= 0
+      "bg-sky-800 text-white hover:bg-sky-600 px-4 gap-2": !hint && numOfHints > 0,
+      "bg-gray-600 hover:bg-gray-600 cursor-not-allowed px-3": !hint && numOfHints <= 0
     }
   );
   return (
@@ -29,9 +28,12 @@ export default function ExtraHint({ hint, step, setHint, numOfHints }: Props) {
         }
         setHint(true);
       }}
-      title={numOfHints <= 0 ? 'You are out of extra hints' : !hint ? 'Click to get an extra hint' : 'Extra hint'}
+      title={hint ? 'Extra hint' : numOfHints <= 0 ? 'You are out of extra hints' : 'Click to get an extra hint'}
     >
-      {hint || (numOfHints <= 0 ? '❌' : `❔x ${numOfHints}`)}
+      {!hint && <img src={!hint && numOfHints > 0 ? "/hint.svg" : "/x.svg"} />}
+      <span>
+        {hint || (numOfHints > 0 && `x ${numOfHints}`)}
+      </span>
     </button>
   );
 }
