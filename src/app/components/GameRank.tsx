95234983b641bcfe7410086359423b6252d54922
KovaKreative <eric.kovalevskyy@gmail.com>
2023-11-02 13:52:36 -0400

    Refactor Game Rank to DRY up the code and make algorithm easier to customize

--- src/app/components/GameRank.tsx
+++ src/app/components/GameRank.tsx
@@ -1,59 +1,62 @@
-import React from 'react';
+"use client";
+
+import classnames from 'classnames';
+import { useState } from 'react';
 
 interface Props {
   hintsUsed: number;
-  retries: number;
+  maxHints: number;
+  retriesLeft: number;
+  maxRetries: number;
   winningTime: number;
 }
 
-export default function GameRank({ hintsUsed, retries, winningTime }: Props) {
-  
-//Declare variables about noumber of stars
-  const ceroStar = 0;  
-  const oneStar = 1;
-  const twoStar = 2;
-  const threeStar = 3;
+export default function GameRank({ hintsUsed, maxHints, retriesLeft, maxRetries, winningTime }: Props) {
 
-  //Function that render the element star
-  const renderStars = (rank: number) => {
-    const stars = Array(rank).fill('⭐'); // Llenar un array con estrellas
-    return stars.map((star, index) => <span key={index} className="star">{star}</span>);
+  const [imagesLoaded, setImagesLoaded] = useState(false);
+
+  const starClass = classnames(
+    "star",
+    {
+      "revealStar": imagesLoaded
+    }
+  )
+
+  const maxStars = 3; ///Number of stars, in case we decide to change it
+
+  /// Get rank based on percentage, with minimum being 1 and maximum being maxStars  
+  const percentRank = function(criteria: number, maxCriteria: number) {
+    const percent = criteria / maxCriteria;
+
+    return Math.max(1, Math.floor(percent * maxStars));
   };
 
-  //Funcrion that evaluate the conditions of game
-  const getGameRank = () => {
-    if (retries === 0 && winningTime <= 60000 && hintsUsed === 0) {
-      return (
-        <div>
-        <h3 className="text-center font-bold mt-2">RANK</h3>
-          <p className="text-center">{renderStars(threeStar)}</p>
-        </div>
-      );
-    } else if (retries === 1 && winningTime <= 120000 && hintsUsed === 1) {
-      return (
-        <div>
-            <h3 className="text-center font-bold mt-2">RANK</h3>
-          <p className="text-center">{renderStars(twoStar)}</p>
-        </div>
-      );
-    } else if (retries === 2 && winningTime <= 200000 && hintsUsed === 2) {
-      return (
-        <div>
-        <h3 className="text-center font-bold mt-2">RANK</h3>
-          <p className="text-center">{renderStars(oneStar)}</p>
-        </div>
-      );
+  const calculateRank = function() {
+
+    if (winningTime === -1 || retriesLeft <= 0) {
+      return 0;
     }
 
-    return (
-      <div>
-         <h3 className="text-center font-bold mt-2">RANK</h3>
-        <p>{renderStars(ceroStar)}</p>
-      </div>
-    );
+    const timeInSeconds = winningTime / 1000;
+    const timeRank = maxStars - Number(timeInSeconds > 30) - Number(timeInSeconds > 60); // Under 30 seconds: 3 stars, under 60 seconds: 2 stars, otherwise 1
+
+    const retriesRank = percentRank(retriesLeft, maxRetries);
+    const hintsRank = percentRank(hintsUsed, maxHints);
+
+    return Math.floor((timeRank + retriesRank + hintsRank) / maxStars);
   };
 
-  return <div>{getGameRank()}</div>;
-}
+  const rank = calculateRank();
 
+  const stars = Array(maxStars).fill("/star-empty.svg").fill("/star-filled.svg", 0, rank);
   
+  const renderedStars = stars.map((star, index) => {
+    return <img key={`star${index}`} className={starClass} alt="Star rank" src={star} loading='eager' onLoad={() => setImagesLoaded(true)} />;
+  });
+
+  return (
+    <div className='flex justify-center'>
+      {renderedStars}
+    </div>
+  );
+};
