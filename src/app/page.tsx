2591608a9345a7124340ad0245ef113a8422a113
mike-dalton <michael.j.dalton@gmail.com>
2023-11-09 08:09:59 -0500

    remove fake delay before merge

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,24 @@
-// "use client"; --- Commented out to test delay code.... bring back in if necessary
+"use client";
 import GameLauncher from "./components/GameLauncher";
 import { hasCookie, getCookies, setCookie  } from 'cookies-next';
-import { useEffect, useState, Suspense } from "react";
-import Loading from "./loading";
+import { useEffect, useState } from "react";
 
-// Imitate delay to test loading component ----- TO BE REMOVED!
-const testDelay = async function(delay: number) {
-  await new Promise(resolve => setTimeout(resolve, delay));
-};
+export default function Home() {
 
-export default async function Home() {
+  const [hasUser, setHasUser] = useState<boolean | null>(true);
 
-  const toShowLoadScreen = await testDelay(2000); // --- Test code only
-
-  // const [hasUser, setHasUser] = useState<boolean | null>(true);
-
-  // useEffect(() => {
-  //   const loggedIn: boolean = hasCookie('username');
-  //   if (!loggedIn) {
-  //     return setHasUser(false);
-  //   } else {
-  //     setHasUser(true);
-  //   }
-  // }, []);
+  useEffect(() => {
+    const loggedIn: boolean = hasCookie('username');
+    if (!loggedIn) {
+      return setHasUser(false);
+    } else {
+      setHasUser(true);
+    }
+  }, []);
   
   return (
-    <Suspense fallback={<Loading />}>
-      <main className="w-screen h-screen">
-        <GameLauncher />
-      </main>
-    </Suspense>
+    <main className="w-screen h-screen">
+      <GameLauncher />
+    </main>
   );
 }
