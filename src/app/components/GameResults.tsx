e833d036c50eb24ad4a54a4a155cd8b9ae9748e8
RJ <randy.j416@yahoo.com>
2023-11-14 16:22:16 -0500

    implement logic to save result image and download

--- src/app/components/GameResults.tsx
+++ src/app/components/GameResults.tsx
@@ -1,26 +1,59 @@
+import { Birthstone_Bounce } from "next/font/google";
 import { useAppSelector } from "../redux/store";
-import html2canvas from 'html2canvas';
+import html2canvas from "html2canvas";
 
 export default function GameResults() {
-
-  const { retries, maxRetries, extraHints, maxExtraHints, winningTime, timeLimit } = useAppSelector(state => state.gameReducer.value);
+  const {
+    retries,
+    maxRetries,
+    extraHints,
+    maxExtraHints,
+    winningTime,
+    timeLimit,
+  } = useAppSelector((state) => state.gameReducer.value);
   const hintsUsed = maxExtraHints - extraHints;
 
-  const timePlayed = (winningTime === -1 || winningTime === null) ? timeLimit : winningTime;
+  const timePlayed =
+    winningTime === -1 || winningTime === null ? timeLimit : winningTime;
 
   const minutes = Math.floor((timePlayed / 1000 / 60) % 60);
   const seconds = Math.floor((timePlayed / 1000) % 60);
-  const formattedTime = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
+  const formattedTime = `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
+
+  let resultImage;
+
+  const saveAsImage = () => {
+    const resultTable = document.getElementById("result");
+    const downloadButton = document.getElementById("download");
+    console.log(resultTable);
+    if (resultTable && downloadButton) {
+      html2canvas(resultTable).then((canvas) => {
+        resultImage = canvas;
+        const imageData = resultImage.toDataURL("image/png")
+        const newData = imageData.replace(/^data:image\/png/, "data:application/octet-stream");
+        downloadButton.setAttribute('href', newData)
+        downloadButton.setAttribute('download', 'result.png')
+        downloadButton.click()
+      });
+    }
+  };
 
   // TODO: Implement sharing function based on this module
 
   return (
     <section className="w-full flex flex-col content-center">
-      <table className="border border-slate-300">
+      <table id="result" className="border border-slate-300">
         <thead>
           <tr>
-            {["Time Played", "Wrong Guesses", "Extra Hints"].map(label => {
-              return <th key={label} className="font-bold py-2 px-4 border-b border-l text-center border-gray-500 text-black">{label}</th>;
+            {["Time Played", "Wrong Guesses", "Extra Hints"].map((label) => {
+              return (
+                <th
+                  key={label}
+                  className="font-bold py-2 px-4 border-b border-l text-center border-gray-500 text-black"
+                >
+                  {label}
+                </th>
+              );
             })}
           </tr>
         </thead>
@@ -32,6 +65,14 @@ export default function GameResults() {
           </tr>
         </tbody>
       </table>
+      <a className="bg-white w-1/5 h-12 mt-2 self-center justify-center content-center" id="download" download={resultImage}>
+        <button
+          onClick={saveAsImage}
+          
+        >
+          Save Result
+        </button>
+      </a>
     </section>
   );
 }
